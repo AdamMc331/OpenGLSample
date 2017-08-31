2c079575612ce2648dc5580a7874ae4007b75de3
AdamMc331 <amcneilly331@gmail.com>
2017-08-30 21:53:05 -0400

    Drew shape onto screen for lesson 3.

--- app/src/main/java/com/adammcneilly/openglsample/MyGLRenderer.kt
+++ app/src/main/java/com/adammcneilly/openglsample/MyGLRenderer.kt
@@ -2,6 +2,8 @@ package com.adammcneilly.openglsample
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import com.adammcneilly.openglsample.shapes.Square
+import com.adammcneilly.openglsample.shapes.Triangle
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -9,9 +11,14 @@ import javax.microedition.khronos.opengles.GL10
  * Basic GLSurfaceView.Renderer that renders just a black screen.
  */
 class MyGLRenderer: GLSurfaceView.Renderer {
+    private lateinit var triangle: Triangle
+    private lateinit var square: Square
+
     override fun onDrawFrame(p0: GL10?) {
         // Redraw color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+        triangle.draw()
     }
 
     override fun onSurfaceChanged(p0: GL10?, p1: Int, p2: Int) {
@@ -22,5 +29,25 @@ class MyGLRenderer: GLSurfaceView.Renderer {
     override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
         // Set the background frame color
         GLES20.glClearColor(0.0F, 0.0F, 0.0F, 1.0F)
+
+        // Initialize shapes here for memory efficiency if they'll never change
+        triangle = Triangle()
+        square = Square()
+    }
+
+    companion object {
+        // Shaders contain OpenGL Shading Language (GLSL) code that must be compiled prior to
+        // using it in the OpenGL ES environment.
+        // To compile this code, create a utility method in your renderer class:
+        fun loadShader(type: Int, shaderCode: String): Int {
+            // Create a vertex shader type or a fragment shader type
+            val shader = GLES20.glCreateShader(type)
+
+            // Add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode)
+            GLES20.glCompileShader(shader)
+
+            return shader
+        }
     }
 }
--- app/src/main/java/com/adammcneilly/openglsample/shapes/Triangle.kt
+++ app/src/main/java/com/adammcneilly/openglsample/shapes/Triangle.kt
@@ -1,5 +1,7 @@
 package com.adammcneilly.openglsample.shapes
 
+import android.opengl.GLES20
+import com.adammcneilly.openglsample.MyGLRenderer
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.FloatBuffer
@@ -14,6 +16,9 @@ import java.nio.FloatBuffer
  */
 class Triangle {
     private val vertexBuffer: FloatBuffer
+    private val program: Int
+    private var positionHandle: Int = 0
+    private var colorHandle: Int = 0
 
     init {
         // Initialize vertex byte buffer for shape coordinates
@@ -30,6 +35,45 @@ class Triangle {
 
         // Set the buffer to read the first coordinate
         vertexBuffer.position(0)
+
+        val vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+        val fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+
+        // Create empty OpenGL ES Program
+        program = GLES20.glCreateProgram()
+
+        // Add the shaders
+        GLES20.glAttachShader(program, vertexShader)
+        GLES20.glAttachShader(program, fragmentShader)
+
+        // Creates OpenGL ES program executables
+        GLES20.glLinkProgram(program)
+    }
+
+    fun draw() {
+        // Add program to OpenGL ES Environment
+        GLES20.glUseProgram(program)
+
+        // Get handle to vertex shader's vPosition member
+        positionHandle = GLES20.glGetAttribLocation(program, "vPosition")
+
+        // Enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(positionHandle)
+
+        // Prepare the triangle coordinate data
+        GLES20.glVertexAttribPointer(positionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, VERTEX_STRIDE, vertexBuffer)
+
+        // Get handle to fragment shader's vColor member
+        colorHandle = GLES20.glGetUniformLocation(program, "vColor")
+
+        // Set color for drawing the triangle
+        GLES20.glUniform4fv(colorHandle, 1, COLOR, 0)
+
+        // Draw triangle
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, VERTEX_COUNT)
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(positionHandle)
     }
 
     companion object {
@@ -43,5 +87,23 @@ class Triangle {
 
         // Set color with red, green, blue, and alpha (opacity) values
         val COLOR = floatArrayOf(0.63671875f, 0.76953125f, 0.22265625f, 1.0f)
+
+        private val VERTEX_COUNT = TRIANGLE_COORDS.size / COORDS_PER_VERTEX
+        private val VERTEX_STRIDE = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
+
+        // Vertex shader is for drawing coordinates
+        private val vertexShaderCode =
+                "attribute vec4 vPosition;" +
+                        "void main() {" +
+                        "  gl_Position = vPosition;" +
+                        "}"
+
+        // Fragment shader is for colors and textures
+        private val fragmentShaderCode =
+                "precision mediump float;" +
+                        "uniform vec4 vColor;" +
+                        "void main() {" +
+                        "  gl_FragColor = vColor;" +
+                        "}"
     }
 }
